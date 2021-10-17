00a9150a90d6c5f26a579bc16fbf2ecde6aa45bd
Shaun <n10537848@qut.edu.au>
2021-10-17 21:31:52 +1000

    setup_environment
    em add them express voi pug

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+const { default: axios } = require("axios");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {});
+
+module.exports = router;
