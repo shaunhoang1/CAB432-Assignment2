482ba269b64f07de07d1ada75b7e8e3a22816e04
Shaun <n10537848@qut.edu.au>
2021-10-29 19:09:17 +1000

    static pages

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,74 +1,74 @@
 // import { getSentiment } from "../helpers/nlp.js";
-const {getSentiment} = require("../helpers/nlp.js");
+const { getSentiment } = require("../helpers/nlp.js");
 const express = require("express");
 const router = express.Router();
 const Twitter = require("twitter");
-const {getSentimentList} = require("../helpers/nlp");
-const {calculateSentiment} = require("../helpers/twitterSentiment");
+const { getSentimentList } = require("../helpers/nlp");
+const { calculateSentiment } = require("../helpers/twitterSentiment");
 
 const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-router.get("/", async (req, res, next) => {
-  res.send({ message: "This is API main page :)" });
-});
+// router.get("/", async (req, res, next) => {
+//   res.send({ message: "This is API main page :)" });
+// });
 
 // To get trending topics
 router.get("/trends", async (req, res, next) => {
-    try {
-        const id = req.query.woeid;
-        const trends = await client.get("trends/place.json", {
-            id,
-        });
-        res.send(trends);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const id = req.query.woeid;
+    const trends = await client.get("trends/place.json", {
+      id,
+    });
+    res.send(trends);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // The route gets the WOEID for a particular location (latitude/longtitude)
 router.get("/near-me", async (req, res, next) => {
-    try {
-        const {lat, long} = req.query;
-        const response = await client.get("/trends/closest.json", {
-            lat,
-            long,
-        });
-        res.send(response);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { lat, long } = req.query;
+    const response = await client.get("/trends/closest.json", {
+      lat,
+      long,
+    });
+    res.send(response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // The route gets the data from the exact key word
 // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
 router.get("/search", async (req, res, next) => {
-    try {
-        const q = req.query.q;
-        const search = await client.get("search/tweets", {
-            q,
-            lang: 'en',
-            count: 100, // Max tweets return. Default:15 - Max:100
-            result_type: "mixed" // Both realtime and popular
-        });
-        const listSentiments = getSentimentList(search.statuses);
-        const result = calculateSentiment(listSentiments)
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const q = req.query.q;
+    const search = await client.get("search/tweets", {
+      q,
+      lang: "en",
+      count: 100, // Max tweets return. Default:15 - Max:100
+      result_type: "mixed", // Both realtime and popular
+    });
+    const listSentiments = getSentimentList(search.statuses);
+    const result = calculateSentiment(listSentiments);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/api/sentiment", (req, res) => {
-    const data = req.body.data;
-    console.log(data);
-    const sentiment = getSentiment(data);
+  const data = req.body.data;
+  console.log(data);
+  const sentiment = getSentiment(data);
 
-    return res.send({sentiment});
+  return res.send({ sentiment });
 });
 
 module.exports = router;
