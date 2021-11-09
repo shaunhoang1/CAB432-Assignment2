b5f79c107f1e3d790fa248122d404d6b6868c5f9
Patrick Ha <51686336+ZadeTheExplorer@users.noreply.github.com>
2021-11-10 03:58:27 +0700

    change sentiment api return

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Pie} from 'react-chartjs-2';
-
+const SENTIMENT_TYPE = {
+    0: 'Neutral',
+    1: 'Positive',
+    2: 'Negative'
+}
 const PieChart = ({sentiment, trend}) => {
     console.log(sentiment);
     const data = {
@@ -8,7 +12,7 @@ const PieChart = ({sentiment, trend}) => {
         datasets: [
             {
                 label: `Sentiment Proportion of Trend ${trend}`,
-                data: Object.values(sentiment),
+                data: Object.values(sentiment.result),
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -25,10 +29,21 @@ const PieChart = ({sentiment, trend}) => {
     };
     return (
         <>
-            <div className='chart'>
-                <h3 className='bar-chart-header'>{`Sentiment Proportion of Trend ${trend}`}</h3>
-                <Pie data={data} type={"pie"}
-                />
+            <div className='chart' >
+                <h3 className='bar-chart-header' style={{textAlign: 'center'}}>{`Sentiment Proportion of Trend ${trend}`}</h3>
+                <div className='container'>
+                    <Pie data={data} type={"pie"} style={{margin: 'auto', height: '15%', width: '15%'}}
+                    />
+                    <div style={{width:'20em', paddingLeft: '1vw', paddingTop: '10vw', textAlign: 'left'}} >
+                    {`Number of tweets: ${sentiment.tweetCount}`}
+                    <br/>
+                    {`Average sentiment score of tweets: ${sentiment.averageScore}`}
+                    <br/>
+                    {`Average sentiment type of tweets: ${SENTIMENT_TYPE[sentiment.averageSentiment]}`}
+                </div>
+                </div>
+
+
             </div>
         </>
     )
--- server/routes/getTweetList.js
+++ server/routes/getTweetList.js
@@ -13,6 +13,64 @@ const twitterClient = new Twitter2({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
+// router.get("/search2", async (req, res, next) => {
+//     try {
+//         const query = req.query.q;
+//         console.log("q", query);
+//         const { data }  = await twitterClient.get(
+//             'tweets/search/recent',
+//             {
+//                 query: 'csgo',
+//                 max_results: 10,
+//                 tweet: {
+//                     fields: [
+//                         'created_at',
+//                         'entities',
+//                         'in_reply_to_user_id',
+//                         'public_metrics',
+//                         'referenced_tweets',
+//                         'source',
+//                         'author_id',
+//                     ],
+//                 },
+//             }
+//         );
+//         const listSentiments = getSentimentList(data);
+//         const result = calculateSentiment(listSentiments);
+//         res.status(200).json(result);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
+router.get("/search/sentiment", async (req, res, next) => {
+    try {
+        const query = req.query.q;
+        console.log("q", query);
+        const { data }  = await twitterClient.get(
+            'tweets/search/recent',
+            {
+                query: 'csgo',
+                max_results: 10,
+                tweet: {
+                    fields: [
+                        'created_at',
+                        'entities',
+                        'in_reply_to_user_id',
+                        'public_metrics',
+                        'referenced_tweets',
+                        'source',
+                        'author_id',
+                    ],
+                },
+            }
+        );
+        // const listSentiments = getSentimentList(search.statuses);
+        // const result = calculateSentiment(listSentiments);
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 router.get("/search", async (req, res, next) => {
     try {
@@ -32,7 +90,7 @@ router.get("/search", async (req, res, next) => {
                     'tweets/search/recent',
                     {
                         query,
-                        max_results: 10, // 100 <PROD> || 10: <DEV>
+                        max_results: 10,
                         tweet: {
                             fields: [
                                 'created_at',
@@ -46,11 +104,68 @@ router.get("/search", async (req, res, next) => {
                         },
                     }
                 );
-                const listSentiments = getSentimentList(data);
-                const result = calculateSentiment(listSentiments);
+                const {sentimentList, averageScore} = getSentimentList(data);
+                const result = calculateSentiment(sentimentList);
 
-                const body = JSON.stringify(result);
+                const outputObject = {result, tweetCount: sentimentList.length, averageScore,averageSentiment:Math.max(...Object.values(sentimentList)) };
+
+                const body = JSON.stringify(outputObject);
                 const objectParams = {Bucket: process.env.AWS_S3_BUCKET_NAME, Key: s3Key, Body: body};
+                const uploadPromise = awsClient.putObject(objectParams).promise();
+                uploadPromise.then(function (data) {
+                    console.log("Successfully uploaded data to " + objectParams.Bucket + "/" + objectParams.Key);
+                }).catch(err => console.error(err));
+
+                return res.status(200).json(outputObject);
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/search-fake", async (req, res, next) => {
+    try {
+        const query = req.query.q.replace(/ /g,"_");
+
+        console.log("search for trends:", query);
+
+
+        const s3Key = `sentiment-${query}`
+        const params = {Bucket: process.env.AWS_S3_BUCKET_NAME, Key: s3Key};
+
+        awsClient.getObject(params, async (err, result) => {
+            // Serve from S3
+            if (result) {
+                console.log("Successfully get data from " + params.Bucket + "/" + params.Key)
+                return res.status(200).json(JSON.parse(result.Body));
+            } else {
+                const { data }  = await twitterClient.get(
+                    'tweets/search/recent',
+                    {
+                        query,
+                        max_results: 10,
+                        tweet: {
+                            fields: [
+                                'created_at',
+                                'entities',
+                                'in_reply_to_user_id',
+                                'public_metrics',
+                                'referenced_tweets',
+                                'source',
+                                'author_id',
+                            ],
+                        },
+                    }
+                ).catch(err => {
+                    console.error(err)
+                    res.status(401).json(err);
+                });
+                const listSentiments = [1,0,0,1,1,0,1,-1,-1,1,-1,1,-1,1,-1,1,-1];
+                const result = await calculateSentiment(listSentiments);
+                console.log("result", result)
+
+                const objectParams = {Bucket: process.env.AWS_S3_BUCKET_NAME, Key: s3Key, Body: JSON.stringify(result)};
                 const uploadPromise = awsClient.putObject(objectParams).promise();
                 uploadPromise.then(function (data) {
                     console.log("Successfully uploaded data to " + objectParams.Bucket + "/" + objectParams.Key);
